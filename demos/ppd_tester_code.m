2712478073660084955c88384939c40c566c02c3
Zoë Rechav <65515046+zoerechav@users.noreply.github.com>
2025-08-05 09:37:55 -0500

    Add files via upload

---
+++ demos/ppd_tester_code.m
@@ -0,0 +1,7 @@
+[S49A_0_dis_table,S49A_0_results_average]=Precursor_Phase_Distance ("S49_mask.png","S49_mask_outline.png","S49_pMap_0.tif",56,4,1030,'S49_0');
+[S49A_1_dis_table,S49A_1_results_average]=Precursor_Phase_Distance ("S49_mask.png","S49_mask_outline.png","S49_pMap_1.tif",56,4,1030,'S49_1');
+[S49A_2_dis_table,S49A_2_results_average]=Precursor_Phase_Distance ("S49_mask.png","S49_mask_outline.png","S49_pMap_2.tif",56,4,1030,'S49_2');
+[S49A_3_dis_table,S49A_3_results_average]=Precursor_Phase_Distance ("S49_mask.png","S49_mask_outline.png","S49_pMap_3.tif",56,4,1030,'S49_3');
+[S49A_5_dis_table,S49A_5_results_average]=Precursor_Phase_Distance ("S49_mask.png","S49_mask_outline.png","S49_pMap_5.tif",56,4,1030,'S49_5');
+
+
